e3864778e605fe42f7111fbad3e960400b487f18
pudisss <pudis.2550@gmail.com>
2021-08-09 12:27:30 +0700

    Portfolio codebase1

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/navbar";
+import UnleashImagination from "./components/unleashimaginaton";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <UnleashImagination />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ react-portfolio/src/components/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../css/index.css";
+
+
+const navbar = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <h1 className="logo">Dev-Pudis.</h1>
+                    
+                </ul>
+            </nav>
+            
+        </div>
+    )
+}
+
+export default navbar
